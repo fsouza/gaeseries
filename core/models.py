0a659e50f46258f7f11b13020baa8e9ec8458a4b
Francisco Souza <fsouza@users.noreply.github.com>
2010-07-31 02:14:18 -0300

    More changes on models.py

--- core/models.py
+++ core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()

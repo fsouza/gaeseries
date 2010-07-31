213cb55109297bfa3c0e7675e0c22c60d6466230
Francisco Souza <fsouza@users.noreply.github.com>
2010-07-31 16:11:34 -0300

    Added login and logout views (and templates)

--- urls.py
+++ urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
+    ('^login/$', 'django.contrib.auth.views.login'),
+    ('^logout/$', 'django.contrib.auth.views.logout'),
 )

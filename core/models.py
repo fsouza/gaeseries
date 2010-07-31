d459a1d6c3976600f7dedb53c202e602f819a25e
Francisco Souza <fsouza@users.noreply.github.com>
2010-07-31 02:15:30 -0300

    Removing everything from master branch

--- core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Post(models.Model):
-    title = models.CharField(max_length = 200)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add = True)
-    user = models.ForeignKey(User)
--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    ('^$', 'django.views.generic.simple.direct_to_template',
-     {'template': 'home.html'}),
-)

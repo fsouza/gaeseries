ee8a5e7aeea73b6b471c1d13c5c27d699684cfdc
Francisco Souza <fsouza@users.noreply.github.com>
2010-07-31 02:12:39 -0300

    Added relationship between Post and User

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User)

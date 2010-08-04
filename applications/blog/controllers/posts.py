45e5198ffaacfe099edcc5bfdadb4f17ec8bf75d
Francisco Souza <fsouza@users.noreply.github.com>
2010-08-03 23:22:00 -0300

    Added action to save a new post

--- applications/blog/controllers/posts.py
+++ applications/blog/controllers/posts.py
@@ -1,3 +1,11 @@
 def index():
     posts = db().select(db.posts.ALL)
     return response.render('posts/index.html', locals())
+
+@auth.requires_login()
+def new():
+    form = SQLFORM(db.posts, fields=['title','content'])
+    if form.accepts(request.vars, session):
+        response.flash = 'Post saved.'
+        redirect(URL('blog', 'posts', 'index'))
+    return response.render('posts/new.html', dict(form=form))

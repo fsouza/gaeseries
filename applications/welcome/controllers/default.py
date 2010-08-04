560238bda9a4715d490aeef61d8a3257b4deac0b
Francisco Souza <fsouza@users.noreply.github.com>
2010-08-03 23:32:21 -0300

    Last fix before deploy

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -13,9 +13,7 @@ def index():
     example action using the internationalization operator T and flash
     rendered by views/default/index.html or views/generic.html
     """
-    response.flash = T('Welcome to web2py')
-    return dict(message=T('Hello World'))
-
+    redirect(URL('blog', 'posts', 'index'))
 
 def user():
     """

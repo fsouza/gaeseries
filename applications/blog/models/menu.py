ff1d94846939a3553611be06f406c03ee3b5e3f3
Francisco Souza <fsouza@users.noreply.github.com>
2010-08-03 22:39:25 -0300

    Changed menu (removed some itens)

--- applications/blog/models/menu.py
+++ applications/blog/models/menu.py
@@ -22,31 +22,3 @@ response.menu = [
 ##
 ## mind that plugins may also affect menu
 ##########################################
-
-response.menu+=[
-    (T('Edit'), False, URL('admin', 'default', 'design/%s' % request.application),
-     [
-            (T('Controller'), False, 
-             URL('admin', 'default', 'edit/%s/controllers/%s.py' \
-                     % (request.application,request.controller=='appadmin' and
-                        'default' or request.controller))), 
-            (T('View'), False, 
-             URL('admin', 'default', 'edit/%s/views/%s' \
-                     % (request.application,response.view))),
-            (T('Layout'), False, 
-             URL('admin', 'default', 'edit/%s/views/layout.html' \
-                     % request.application)),
-            (T('Stylesheet'), False, 
-             URL('admin', 'default', 'edit/%s/static/base.css' \
-                     % request.application)),
-            (T('DB Model'), False, 
-             URL('admin', 'default', 'edit/%s/models/db.py' \
-                     % request.application)),
-            (T('Menu Model'), False, 
-             URL('admin', 'default', 'edit/%s/models/menu.py' \
-                     % request.application)),
-            (T('Database'), False, 
-             URL(request.application, 'appadmin', 'index')),
-            ]
-   ),
-  ]

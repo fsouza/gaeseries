6127db6e5839e6776f835d8db734be92b95fd294
Francisco Souza <fsouza@users.noreply.github.com>
2010-09-08 01:08:55 -0300

    Added URL mapping for the NewPostHandler

--- app/apps/blog/urls.py
+++ app/apps/blog/urls.py
@@ -3,6 +3,7 @@ from tipfy import Rule
 def get_rules(app):
     rules = [
         Rule('/posts', endpoint='post-listing', handler='apps.blog.handlers.PostListingHandler'),
+        Rule('/posts/new', endpoint='new-post', handler='apps.blog.handlers.NewPostHandler'),
     ]
 
     return rules

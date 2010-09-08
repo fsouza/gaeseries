f47871717af932274e064dd3030bc195a0b6c045
Francisco Souza <fsouza@users.noreply.github.com>
2010-09-08 00:56:16 -0300

    Added the PostForm class to the handlers.py module

--- app/apps/blog/handlers.py
+++ app/apps/blog/handlers.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from tipfy import RequestHandler
 from tipfy.ext.jinja2 import render_response
+from tipfy.ext.auth import login_required, AppEngineAuthMixin
+from tipfy.ext.session import AllSessionMixins, SessionMiddleware
+from tipfy.ext.wtforms import Form, fields, validators
 from models import Post
 
 class PostListingHandler(RequestHandler):
     def get(self):
         posts = Post.all()
         return render_response('list_posts.html', posts=posts)
+
+class PostForm(Form):
+    csrf_protection = True
+    title = fields.TextField('Title', validators=[validators.Required])
+    content = fields.TextAreaField('Content', validators=[validators.Required])
+
